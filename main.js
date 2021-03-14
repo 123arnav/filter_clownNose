483d418afaa8424a7e3f351b76a9e21f10c12ecf
123arnav <74090758+123arnav@users.noreply.github.com>
2021-03-14 11:54:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(400,400)
+canvas.position(650,300)
+video=createCapture(VIDEO)
+video.hide();
+}
+function draw(){
+image(video,0,0,400,400);
+}
